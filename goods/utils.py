4a15689bd00a112054cd8f6daee9f03fcf8383e9
DariaZiboorva <is50_d.v.ziborova@mpt.ru>
2025-05-15 02:01:45 +0300

    login form ready

--- goods/utils.py
+++ goods/utils.py
@@ -5,12 +5,12 @@ from goods.models import Products
 
 
 def q_search(query):
-    if query.isdigit() and len(query) <=5:
+    if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     
-    vector = SearchVector('name','description')
+    vector = SearchVector('name', 'description')
     query = SearchQuery(query)
-    return Products.objects.annotate(rank=SearchRank(vector,query)).filter(rank__gt=0).order_by('-rank')
+    return Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
     
     # keywords=[word for word in query.split() if len(word)>2]
 
--- goods/views.py
+++ goods/views.py
@@ -5,12 +5,12 @@ from django.template import context
 from goods.models import Products
 from goods.utils import q_search
 
-def catalog(request,category_slug=None):
+def catalog(request, category_slug=None):
 
     page=request.GET.get('page',1)
-    on_sale=request.GET.get('on_sale',None)
-    order_by=request.GET.get('order_by',None)
-    query=request.GET.get('q',None)
+    on_sale=request.GET.get('on_sale', None)
+    order_by=request.GET.get('order_by', None)
+    query=request.GET.get('q', None)
 
 
     if category_slug == 'all':
