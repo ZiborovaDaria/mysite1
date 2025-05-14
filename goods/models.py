daedb61fd314e74d4a02d2795d79cc072afe8847
DariaZiboorva <is50_d.v.ziborova@mpt.ru>
2025-05-14 04:54:13 +0300

    full text search + postres sql data base

--- goods/models.py
+++ goods/models.py
@@ -21,7 +21,7 @@ class Products(models.Model):
     description=models.TextField(blank=True, null=True, verbose_name='ОписаниеL')
     image = models.ImageField(upload_to='gooods_images',blank=True, null=True, verbose_name='Изображение')
     price=models.DecimalField(default=0.00,max_digits=7,decimal_places=2, verbose_name='Цена')
-    discount=models.DecimalField(default=0.00,max_digits=7,decimal_places=2, verbose_name='Скидка в %')
+    discount=models.DecimalField(default=0.00,max_digits=4,decimal_places=2, verbose_name='Скидка в %')
     quantity=models.PositiveIntegerField(default=0, verbose_name='Количество')
     category=models.ForeignKey(to=Categories, on_delete=models.PROTECT,verbose_name= "Категория")
 
@@ -29,7 +29,6 @@ class Products(models.Model):
         db_table='product'
         verbose_name= "Продукт"
         verbose_name_plural = 'Продукты'
-        ordering=('id,')
 
     def __str__(self):
         return f'{self.name} Количество - {self.quantity}'
